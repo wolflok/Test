e8bb0f81a6e3363286149372331c31472bb4a984
wolflok <wolflok@gmail.com>
2017-07-29 13:33:46 -0400

    problem 3

--- prime.py
+++ prime.py
@@ -11,33 +11,9 @@ def checkprime(target,denom):
 			break
 		denom=denom+1
 		limit=target//denom
-	#print ("limit is ", limit)
-	#print ("denom is ", denom)
 	if flag==False:
 		return False
 	else:
 		return True
 		
-
-#primeList = []
-#for i in range(2,100):
-#	if checkprime(i) == True:
-#		primeList.append(i)
-
-
-#flag=True
-#number=101
-#denom=2
-#limit=number
-#while denom < limit:
-#	if (number % denom) ==0:
-#		print (number, " is not prime")
-#		flag=False
-#		break
-#	denom=denom+1
-#	limit=limit//denom
-#
-#if flag == True:
-#	print (number , "  is prime")
-
 
