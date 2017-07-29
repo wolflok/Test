0e88738aea8d9bf90c8d069b312594751854bcfd
chifung.cheng <chifung.cheng@gmail.com>
2017-07-29 13:14:30 -0400

    formatting (use 4 spaces instead of tabs -- or may cause trouble!)

--- Euler2.py
+++ Euler2.py
@@ -3,11 +3,11 @@ next_val = 2
 cur_val = 1
 sum = 0
 while next_val < 4000000:
-	if next_val % 2 == 0:
-		sum = sum + next_val
-	tmp = next_val
-	next_val=next_val + cur_val
-	cur_val=tmp
+    if next_val % 2 == 0:
+        sum = sum + next_val
+    tmp = next_val
+    next_val=next_val + cur_val
+    cur_val=tmp
 
 print (sum)
 
