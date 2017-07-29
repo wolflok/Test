6c6f1fc712f5d6d5904e1cb505645de599643b16
chifung.cheng <chifung.cheng@gmail.com>
2017-07-29 13:14:02 -0400

    one-liner for problem1

--- Euler1.py
+++ Euler1.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-sum=0
-for i in range(1,1000):
-	print ("sum starts with ",sum)
-	if i % 3 == 0:
-		sum = sum + i
-	elif i % 5 == 0:
-		sum = sum + i
-print (sum)
-
+#sum=0
+#for i in range(1,1000):
+#	print ("sum starts with ",sum)
+#	if i % 3 == 0:
+#		sum = sum + i
+#	elif i % 5 == 0:
+#		sum = sum + i
+#print (sum)
+print(sum((x for x in range(1,1000) if (x % 3 == 0 or x % 5 ==0))))
